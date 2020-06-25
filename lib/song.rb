431c251d8f51c398ca64f5844331457cf48d9aae
Jessica Pereira <jcpereira070@gmail.com>
2020-06-25 16:35:28 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -35,18 +35,6 @@ self.all.find{|item|item.name == name}end
 end
 
 def find_or_create_by_name(name)
-			song = find_by_name(name)
-			if song == nil
-				self.create(name)
-			else
-				song
-			end
-		end
+			song.find_by_name(song_name) = song.find_by_name(song_name)
 
-	end
-
-	module ClassMethods
-
-		def destroy_all
-			self.all.clear
 		end
