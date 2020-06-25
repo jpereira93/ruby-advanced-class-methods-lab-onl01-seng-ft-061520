13402253198a5b26cdafdcdb18e3b243662d927c
Jessica Pereira <jcpereira070@gmail.com>
2020-06-25 16:18:56 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -29,7 +29,7 @@ class Song
 end
 
 def self.find_by_name(song_name)
-self.all.find{|song_name| self.name = song_name}  
+self.all.find{|song_name| self.name = song_name}
   song
 end
   # def self.new_by_name
